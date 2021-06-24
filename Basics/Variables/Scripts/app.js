1a8e5fdf38df5b1679a6416d0c1e358b8f2e196f
Mustafa Karacabey <mustafa.karacabey@outlook.com>
2021-06-24 14:13:42 +0300

    [JS-2]

---
+++ Basics/Variables/Scripts/app.js
@@ -0,0 +1,27 @@
+var name = 'Mustafa';
+let surName = 'Karacabey';
+const PI = "3.14";
+let age = 18;
+let isActive = true;
+let job = null;
+let car;//Undefined
+let names = ['Mustafa','Kadir'];
+let address = {
+    key : "İstanbul"
+}
+
+let number2= "12";
+
+
+
+console.log("Name variable type is : " + typeof(name) );
+console.log("Age variable type is : " + typeof (age));
+console.log("İsActive variable type is : " + typeof(isActive));
+console.log("Job variable type is : " + typeof(job));
+console.log("Car variable type is : " + typeof(car));
+console.log("Names variable is : " + typeof(names));
+console.log("address variable is : " + typeof(address))
+console.log("Conver number2 string to int" + typeof(parseInt(number2)));
+console.log(name);
+console.log(surName);
+console.log(PI)
